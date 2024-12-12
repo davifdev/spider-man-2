5a2889b3857662792474add0372dcaf096a26a7e
Davi Fernandes <davi.fer159@gmail.com>
2024-12-12 10:50:16 -0300

    responsivo

--- js/main.js
+++ js/main.js
@@ -10,4 +10,24 @@ const swiper = new Swiper(".slide-video", {
     el: ".s-videos .swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+    },
+    550: {
+      slidesPerView: 1.5,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2.1,
+    },
+    1024: {
+      slidesPerView: 3.1,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
 });
